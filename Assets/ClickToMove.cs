1919c1bd71e8e7449ff2ddac34afe40d8de6b3d8
Adhithyan <madhithyan00@gmail.com>
2026-01-17 17:50:47 +0530

    Initial Unity project commit

---
+++ Assets/ClickToMove.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickToMove : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
